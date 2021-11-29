29fd5b99ee26ce4c990dd018fbc08c005e128e8b
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-11-29 14:26:06 -0500

    Initial Commit

---
+++ Physics/PhysicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicsApp.swift
+//  Physics
+//
+//  Created by Jacobo de Juan Millon on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct PhysicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
